8d60470f8f3b4efc1a08a66b97342f5ecbfb35f3
Michael Ionov <imichaelionov@gmail.com>
2023-06-03 13:51:40 +0300

    feat: add go main file

--- go/cmd/projector/main.go
+++ go/cmd/projector/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/invm/projector-cli-app/go/pkg/config"
+	"github.com/invm/projector-cli-app/go/pkg/projector"
 )
 
 func main() {
@@ -12,10 +14,31 @@ func main() {
 	if err != nil {
 		log.Fatal("Unable to get opts: ", err)
 	}
-  fmt.Printf("opts :%+v\n", opts)
-  config, err := config.NewConfig(opts)
-  if err != nil {
-    log.Fatal("Unable to get config: ", err)
-  }
-  fmt.Printf("config :%+v\n", config)
+	cfg, err := config.NewConfig(opts)
+	if err != nil {
+		log.Fatal("Unable to get config: ", err)
+	}
+	proj := projector.NewProjector(cfg)
+	if cfg.Operation == config.Print {
+		if len(cfg.Args) == 0 {
+			data := proj.GetValueAll()
+			jsonString, err := json.Marshal(data)
+			if err != nil {
+				log.Fatal("This line should never be reached")
+			}
+			fmt.Printf("%v", string(jsonString))
+		} else if value, ok := proj.GetValue(cfg.Args[0]); ok {
+			fmt.Printf("%v", string(value))
+		}
+	}
+
+	if cfg.Operation == config.Add {
+		proj.SetValue(cfg.Args[0], cfg.Args[1])
+		proj.Save()
+	}
+
+	if cfg.Operation == config.Delete {
+		proj.DelValue(cfg.Args[0])
+		proj.Save()
+	}
 }
